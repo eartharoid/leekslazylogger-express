59dffa1d5cb304c3d87296f1b4d3927308171fa3
Isaac (eartharoid) <contact@eartharoid.me>
2020-10-03 23:10:18 +0100

    2.0.4

--- test/test.js
+++ test/test.js
@@ -1,1 +1,20 @@
-console.log('Please run tests individually');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const Logger = require('../lib');
+const log = new Logger({
+	express: {
+		format: '{method} ({protocol}) &7{path} {status-colour}{status} {time-color}({time})'
+	}
+});
+
+app.use(log.express); // logger
+
+app.get('*', (req, res) => {
+	res.send(log.options);
+});
+
+app.listen(port, () => {
+	log.info(`Example app listening at http://localhost:${port}`);
+});
